ba04e7b21a4f9ccad42f39fa9e06ace1681fe3be
Daniel-TheProgrammer <njid18753@gmail.com>
2021-08-14 04:16:43 +0100

    header COntent for all Pages

--- DTP/index.php
+++ DTP/index.php
@@ -12,17 +12,10 @@ include 'header.php';
 <p>In this video, we demonstrate the safe and proper use of the eDTP.</p>
 
 <p>
-The eDTP is one of the most advanced and widely used stairlifts in the world, helping hundreds of thousands of people worldwide to safely and comfortably use their stairs and enjoy their homes again.
+Every aspect of the eDTP has been designed to the highest standards of comfort and safety. Our years of experience have helped us to create a Lift that is built purely with the end-user in mind.
 </p>
 <p>
-Every aspect of the eDTP has been designed to the highest standards of comfort and safety. Our years of experience have helped us to create a stairlift that is built purely with the end-user in mind.
-</p>
-<p>
-The eDTP is powered by a maintenance-free DC powered battery pack, which ensures uninterrupted use, even in the event of a power outage.
-With a built-in soft start and stop mechanism, the eDTP provides a smooth and comfortable ride every time.
-</p>
-<p>
-Another feature pioneered by EDTP is the on-board diagnostic control panel which alerts you to any potential problems before they arise, (e.g. the stairlift has accidentally been switched off or incorrectly parked).
+Another feature pioneered by EDTP is the on-board diagnostic control panel which alerts you to any potential problems before they arise, (e.g. the lift has accidentally been switched off or incorrectly parked).
 </p>
 
 <?php
